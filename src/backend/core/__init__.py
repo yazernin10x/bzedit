a42e9e561aa197e8fa407139b160f1e2038c74e2
Ali YARO <aliyaro104@gmail.com>
2025-01-19 16:03:58 +0100

    Refactoring of imports

--- src/backend/core/__init__.py
+++ src/backend/core/__init__.py
@@ -1,1 +1,18 @@
+from ._meta import DocstringInheritanceMeta
+from ._abstractengine import AbstractEngine
+from ._abstractselection import AbstractSelection
+from ._selection import Selection
+from ._engine import Engine
 
+__all__ = [
+    "DocstringInheritanceMeta",
+    "AbstractEngine",
+    "AbstractSelection",
+    "Selection",
+    "Engine",
+]
+
+# Adjust __module__ to show objects as part of the current package
+for value in list(locals().values()):
+    if getattr(value, "__module__", "").startswith(__name__):
+        value.__module__ = __name__
