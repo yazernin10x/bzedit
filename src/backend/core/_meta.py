d5d587113505e3ce33f71eec6248f61e1e088594
Ali YARO <aliyaro104@gmail.com>
2025-01-19 15:56:57 +0100

    add DocstringInheritanceMeta class to enable automatic docstring inheritance across subclasses.

---
+++ src/backend/core/_meta.py
@@ -0,0 +1,12 @@
+"""Define metaclass"""
+
+
+class DocstringInheritanceMeta(type):
+    def __new__(cls, name, bases, dct):
+        for attr_name, attr_value in dct.items():
+            if isinstance(attr_value, property) or callable(attr_value):
+                for base in bases:
+                    if base_attr := getattr(base, attr_name, None):
+                        attr_value.__doc__ = base_attr.__doc__
+
+        return super().__new__(cls, name, bases, dct)
