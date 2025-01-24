be9fb4c0243f9c2e6791187d35ec74e43decdd67
Ali YARO <aliyaro104@gmail.com>
2025-01-24 02:37:08 +0100

    Simplification of selection tests.

--- test/src/backend/core/test_selection.py
+++ test/src/backend/core/test_selection.py
@@ -5,30 +5,80 @@ import pytest
 
 
 class TestSelection:
-    def test_begin_index_valid(self, selection):
-        selection.begin = 20
-        assert selection.begin == 20
+    """Test the Selection class
 
-    @pytest.mark.parametrize("index", [-25, 5, 110])
-    def test_begin_index_invalid(self, selection, index):
-        with pytest.raises(IndexError, match="Invalid selection begin index"):
-            selection.begin = index
+    For the tests of the `_check_index` method, different scenarios are
+    evaluated, whether the buffer is empty or not.
+    To get a value beyond ``buffer_end``, whether the buffer is empty or not,
+    we use ``BUFFER_END_NON_EMPTY_TEXT + 1``.
+    """
 
-    def test_end_index_valid(self, selection):
-        selection.end = 49
-        assert selection.end == 50
+    TEXT = "I'm content in the buffer"
+    SELECTION_START = 10
+    SELECTION_END = 10
+    BUFFER_END_EMPTY_TEXT = 0
+    BUFFER_END_NON_EMPTY_TEXT = len(TEXT)
+    SELECTION_START_AFTER_CREATION = 0
 
-    @pytest.mark.parametrize("index", [-25, 9, 102])
-    def test_end_index_invalid(self, selection, index):
-        with pytest.raises(IndexError, match="Invalid selection end index"):
-            selection.end = index
+    def test_start_getter(self, selection: Selection) -> None:
+        assert selection.start == self.SELECTION_START_AFTER_CREATION
 
-    def test_buffer_begin(self, engine):
+    def test_start_setter(self, mocker: MockerFixture, selection: Selection) -> None:
+        selection._check_index = mocker.Mock(return_value=None)  # type: ignore[method-assign]
+        selection.start = self.SELECTION_START
+        assert selection.start == self.SELECTION_START
+
+    def test_end_getter(self, selection: Selection) -> None:
+        assert selection.end == self.SELECTION_START_AFTER_CREATION
+
+    def test_end_setter(self, mocker: MockerFixture, selection: Selection) -> None:
+        selection._check_index = mocker.Mock(return_value=None)  # type: ignore[method-assign]
+        selection.end = self.SELECTION_END
+        assert selection.end == self.SELECTION_END + 1
+
+    def test_buffer_start(self, selection: Selection) -> None:
+        assert selection.buffer_start == self.SELECTION_START_AFTER_CREATION
+
+    def test_buffer_end(self, mocker: MockerFixture) -> None:
+        expected = "I'm buffer"
+        engine = mocker.patch("src.backend.core.Engine", autospec=True)
+        engine = engine.return_value
+        p = mocker.PropertyMock(return_value=expected)
+        type(engine).buffer = p
+
         sut = Selection(engine=engine)
-        assert sut.buffer_begin == 0
+        assert sut.buffer_end == len(expected)
 
-    @pytest.mark.parametrize("engine", ["", "I'm content"], indirect=True)
-    def test_buffer_end(self, engine):
-        sut = Selection(engine=engine)
-        expected = (len(engine.content) - 1) if engine.content else 0
-        sut.buffer_end == expected
+    @pytest.mark.parametrize("index", [-2, BUFFER_END_NON_EMPTY_TEXT + 1])
+    @pytest.mark.parametrize(
+        "selection",
+        [(BUFFER_END_EMPTY_TEXT, ""), (BUFFER_END_NON_EMPTY_TEXT, TEXT)],
+        indirect=True,
+    )
+    def test_check_index_outbound(self, selection: Selection, index: int) -> None:
+        """Test the out-of-bounds index."""
+
+        with pytest.raises(IndexError, match="Invalid selection index"):
+            selection._check_index(index)
+
+    @pytest.mark.parametrize("index", [0])
+    @pytest.mark.parametrize(
+        "selection",
+        [(BUFFER_END_EMPTY_TEXT, ""), (BUFFER_END_NON_EMPTY_TEXT, TEXT)],
+        indirect=True,
+    )
+    def test_check_index_empty_buffer_zero_index(
+        self, selection: Selection, index: int
+    ) -> None:
+        """Test the index 0."""
+        assert selection._check_index(index) is None  # type: ignore[func-returns-value]
+
+    @pytest.mark.parametrize("index", [BUFFER_END_NON_EMPTY_TEXT - 1])
+    @pytest.mark.parametrize(
+        "selection", [(BUFFER_END_NON_EMPTY_TEXT, TEXT)], indirect=True
+    )
+    def test_check_index_empty_buffer_positive_inbound(
+        self, selection: Selection, index: int
+    ) -> None:
+        """Test the index located within the buffer's index range."""
+        assert selection._check_index(index) is None  # type: ignore[func-returns-value]
