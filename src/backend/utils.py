606991e7c891ef6b55a1323140171a530d042f3c
Ali YARO <aliyaro104@gmail.com>
2025-01-25 22:25:53 +0100

    Added parameters to the reassign_module_names function and added the py.typed file.

--- src/backend/utils.py
+++ src/backend/utils.py
@@ -1,9 +1,24 @@
-def reassign_module_names() -> None:
+from typing import Any
+
+from typeguard import typechecked
+
+
+@typechecked
+def reassign_module_names(package_name: str, locals_: dict[str, Any]) -> None:
     """Reassign the `__module__` attribute of objects to the current package.
 
     This function iterates through all local objects and adjusts their
     `__module__` attribute if it starts with the name of the current module.
     This ensures that objects are displayed as part of the current package.
+
+      Parameters
+    ----------
+    package_name : str
+        The name of the current package to assign to the `__module__` attribute
+        of relevant objects.
+
+    locals_ : dict[str, Any]
+        A dictionary representing the local objects.
 
     Notes
     -----
@@ -15,6 +30,6 @@ def reassign_module_names() -> None:
     --------
     >>> reassign_module_names()
     """
-    for value in list(locals().values()):
-        if getattr(value, "__module__", "").startswith(__name__):
-            value.__module__ = __name__
+    for value in list(locals_.values()):
+        if getattr(value, "__module__", "").startswith(package_name):
+            value.__module__ = package_name
